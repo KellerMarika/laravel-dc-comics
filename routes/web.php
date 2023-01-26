cf46895acd482acacb53f41dfe066e00b788e3b5
KellerMarika <marikakeller92@gmail.com>
2023-01-26 17:54:20 +0100

    show ok

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', [ComicController::class, 'index'])->name('comics.home');
+Route::get('/', [ComicController::class, 'index'])->name('comics.index');
+
+Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
